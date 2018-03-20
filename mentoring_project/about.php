4988f35c00c04b49a02a1f7e4518977dd4db6dfa
levanath01 <35432210+levanath01@users.noreply.github.com>
2018-03-20 21:34:39 +0530

    Add files via upload

---
+++ mentoring_project/about.php
@@ -0,0 +1,315 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Academic A Education Category Flat Bootstrap Responsive Website Template | About :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Academic Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!---->
+<script src="js/bootstrap.min.js"></script>
+<link href='//fonts.googleapis.com/css?family=Catamaran:400,100,300,500,700,600,800,900' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+ <div class="header head1">
+	<div class="container">
+		<div class="head-top">
+			<div class="logo">
+				<a href="index.php"><img src="images/TOP-BANNER-NEW-black1.jpg" alt="" title="Academic"></a>
+			</div>
+			<div class="login">
+				<ul class="nav-login">
+					<li><a href="#" data-toggle="modal" data-target="#myModal1"><b>Login</b></a></li>
+					
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+		<!-- login -->
+			<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content modal-info">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body modal-spa">
+							<div class="login-grids">
+							
+									<div class="login-right">
+										<form action="login_action.php" method="post">
+											<h3>Signin with your account </h3>
+											<input type="text" value="Enter your Username/Email" name="username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your username or Email';}" required="">	
+											<input type="password" value="Password" name="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" required="">	
+											<h4><a href="#">Forgot password</a> / <a href="#">Create new password</a></h4>
+											<div class="single-bottom">
+												<input type="checkbox"  id="brand" value="">
+												<!--<label for="brand"><span></span>Remember Me.</label>-->
+											</div>
+											<input type="submit" value="Log In" >
+										</form>
+									</div>
+									
+								<p>By logging in you agree to our <span>Terms and Conditions</span> and <span>Privacy Policy</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //login -->
+			<!-- signup -->
+			<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content modal-info">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body modal-spa">
+							<div class="login-grids">
+							
+									<div class="login-right">
+										<form action="#" method="post">
+											<h3>Create your account </h3>
+											<input type="text" value="Name" name="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+											<input type="text" value="Mobile number" name="Mobile number" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Mobile number';}" required="">
+											<input type="text" value="Email id" name="Email id" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email id';}" required="">	
+											<input type="password" value="Password" name="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" required="">	
+											
+											<input type="submit" value="CREATE ACCOUNT" >
+										</form>
+									</div>
+								
+								<p>By logging in you agree to our <span>Terms and Conditions</span> and <span>Privacy Policy</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //signup -->
+	
+			<div class="nav-top">
+			<div class="container">
+				<div class="nav1">
+					<div class="navbar1">
+					<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" >
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+		 
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav cl-effect-8">
+								<li ><a  href="index.php">Home </a></li>
+								<li><a class="active" href="about.php">About</a></li>
+								
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div>
+					<!-- start search-->
+					<ul class="social-ic">
+						<li><a href="#"><i></i></a></li>
+						<li><a href="#"><i class="ic"></i></a></li>
+						<li><a href="#"><i class="ic1"></i></a></li>
+						<li><a href="#"><i class="ic2"></i></a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div> 
+			</div> 
+		</div> 
+</div> 
+<!-- about -->
+<div class="about">
+				<div class="container">
+				<div class="about-head">
+					<h2>About Us</h2>
+					<p> St. Joseph's College of Engineering and Technology, Palai (also referred to as SJCET Palai) is an Indian private college managed by the Syro-Malabar Catholic Diocese of Palai, located in Palai, Kerala, India.</p>
+				</div>
+					<div class="about-grids">
+					<div class="col-md-5 about-grid1">
+						
+							
+							<div class="about-img">
+								<img src="images/DOt81EIVoAA5KlV.jpg" class="img-responsive" alt="">
+							</div>
+							
+							
+						</div>
+						<div class="col-md-7 about-grid">
+							<h3>Get to know us more!</h3>
+							<h5>SJCET is an AICTE approved college offering professional degree programs in engineering and management courses.[2] SJCET Palai is a constituent college of Mahatma Gandhi University, Kottayam and A P J Abdul Kalam Technological University.[3][4] Four of its engineering undergraduate degree programs- Applied Electronics and Instrumentation Engineering, Computer Science and Engineering, Electronics and Communications Engineering and Mechanical Engineering- are accredited by the National Board of Accreditation, AICTE.[5] This college is an ISO 9001:2008 certified institution which regularly conducts internal and external audits.</h5>
+							<!--<p>Nullam velit metus tristique id purus eget consequat congue loreml orem ipsum dolor sit amet consectetur adipiscing elit. Praesent vesti Cum sociis natoque penatibus et magnis dis parturient montes ascetur ridiculus .</p>-->
+							<div class="about-top">
+								
+								
+								<div class="clearfix"></div>
+							</div>
+							<div class="about-top">
+								
+								
+								<div class="clearfix"></div>
+							</div>
+						</div>
+						
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+<!---->
+	
+<div class="test">
+		<div class="container">
+		<h3>Testimonials</h3>
+			<div  class=" test-grid-1" >
+				<div class="col-md-6 test-wrapper">
+					<img src="images/te.jpg" alt="" class="img-responsive">
+					<div class="test-grid">
+					<div class="test-gr">
+						<h4>Andery</h4>
+						<span>Ceo</span>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="col-md-6 test-wrapper">
+					<img src="images/te1.jpg" alt="" class="img-responsive">
+					<div class="test-grid">
+					<div class="test-gr">
+						<h4>Hailey</h4>
+						<span> Principal</span>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			<div class="clearfix"> </div>
+			</div>
+			<div  class=" test-grid-2" >
+				<div class="col-md-6 test-wrapper">
+					<img src="images/te2.jpg" alt="" class="img-responsive">
+					<div class="test-grid">
+					<div class="test-gr">
+						<h4>Quincy</h4>
+						<span>Manager</span>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="col-md-6 test-wrapper">
+					<img src="images/te3.jpg" alt="" class="img-responsive">
+					<div class="test-grid">
+					<div class="test-gr">
+						<h4>Barb</h4>
+						<span>professor</span>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+
+<!---->
+<!--<div class="content-bottom">
+	<h1>Gallery</h1>
+		<div class="content-in">
+            <div class="port effect-1">
+                <div class="image-box">
+                   <img src="images/fi.jpg" alt="" class="img-responsive">
+               </div>
+                <div class="text-desc">
+                    <h6>Education</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .</p>
+                </div>
+           </div>
+         </div>
+		<div class="content-in">
+            <div class="port effect-1">
+                <div class="image-box">
+                    <img src="images/fi1.jpg" alt="" class="img-responsive">
+                </div>
+				<div class="text-desc">
+                    <h6>Education</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .</p>	
+                </div>
+            </div>
+        </div>
+		<div class="content-in">
+            <div class="port effect-1">
+                <div class="image-box">
+                    <img src="images/fi3.jpg" alt="" class="img-responsive">
+                </div>
+				<div class="text-desc">
+					<h6>Education</h6>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .</p>   	
+				</div>
+			</div>
+        </div>
+		<div class="content-in">
+            <div class="port effect-1">
+                <div class="image-box">
+                    <img src="images/fi2.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="text-desc">
+                    <h6>Education</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .</p>	
+                </div>
+            </div>
+        </div>
+		<div class="content-in">
+            <div class="port effect-1">
+                <div class="image-box">
+                    <img src="images/fi4.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="text-desc">
+                    <h6>Education</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .</p>	
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+</div>-->
+<!---->
+<!--footer-->
+<div class="footer">
+	<div class="container">
+		<div class="col-md-6 footer-left">
+			<h3>Contact Us</h3>
+			<ul class="con-icons">
+				<li><span class="glyphicon glyphicon-phone" aria-hidden="true"></span>+123 456 7890</li>
+				<li><a href="mailto:info@example.com"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>www.sjcetpalai.ac.in/
+</a></li>
+			</ul>
+			<p class="copy-right">© 2016 Academic. All rights reserved  <a href=""></a></p>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--//footer-->
+</body>
+</html>
